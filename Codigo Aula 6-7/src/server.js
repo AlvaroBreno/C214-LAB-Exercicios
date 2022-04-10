de984ff05fdf20dfe95c4dca4c604acb5ea3a4f3
AlvaroBreno <93884431+AlvaroBreno@users.noreply.github.com>
2022-04-10 20:29:53 -0300

    subindo aula 7 com os testes

--- Codigo Aula 6-7/src/server.js
+++
@@ -1,9 +0,0 @@
-require('./src/infra/database.js');
-const app = require('./src/infra/rest_server');
-const port = 6000
-
-app.listen(port, () => {
-    console.log('Use http://localhost:' + port);
-    console.log('REST server running on port ' + port + '...');
-
-});
